666c4ad0b57e2d91fd6647118b5aebed5687fd33
Giuseppe Barillari <joe2k01dev@gmail.com>
2021-04-22 16:50:18 +0100

    python-game-pc: Update minigame template to a more up to date standard
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- template/template-minigame.py
+++ template/template-minigame.py
@@ -1,4 +1,4 @@
-import pygame, utils, template-minigame_files.sprite1, template-minigame_files.sprite2, sys, time
+import pygame, utils, template-minigame_files.sprite1, sys, time
 from random import randint
 
 # Global variables
@@ -15,26 +15,24 @@ def end_anim(screen, win):
     """
     global X
     global Y
-    global car
     global background
 
     if win:
-        img = pygame.image.load("engine_files" + utils.sep + "win.png").convert_alpha()
+        img = pygame.image.load("template-minigame_files" + utils.sep + "win.png").convert_alpha()
     else:
-        img = pygame.image.load("engine_files" + utils.sep + "lose.png").convert_alpha()
+        img = pygame.image.load("template-minigame_files" + utils.sep + "lose.png").convert_alpha()
 
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
-    screen.blit(car, (X, Y))
     img_rect = img.get_rect()
     img_rect.center = (utils.width / 2, utils.height/2)
     screen.blit(img, img_rect)
 
     # Sound
     if win:
-        pygame.mixer.Sound.play(utils.win_sound)
+        pygame.mixer.Sound.play(utils.win_sound).set_volume(utils.volume)
     else:
-        pygame.mixer.Sound.play(utils.lose_sound)
+        pygame.mixer.Sound.play(utils.lose_sound).set_volume(utils.volume)
 
     # Info
     utils.draw_points(screen)
@@ -43,7 +41,7 @@ def end_anim(screen, win):
     pygame.display.flip()
     pygame.time.wait(3000)
 
-def engine_game(screen, get_data):
+def templateminigame_game(screen, get_data, decrease_lives):
     """
     This function handles the 'template-minigame' minigame.
     - screen: pygame.display to draw to.
@@ -54,13 +52,12 @@ def engine_game(screen, get_data):
     global Y
     global background
 
-    # Initialise here the points variables
-    points = 0
-    other_useful_var = 0
+    # Initialise points variable
+    points_counter = utils.points
 
     # Load main sprite:
     # Use this method only if the sprite is a single image.
-    # If you want to animate it, jump to line #
+    # If you want to animate it, jump to line #68
     # to see how to import an animated sprite.
     main_sprite = pygame.image.load("template-minigame_files" + utils.sep + "main.png").convert_alpha()
 
@@ -83,16 +80,12 @@ def engine_game(screen, get_data):
     utils.text_colour = (255, 255, 255) # Set the text colour for the minigame
     while True:
         # Game logic
-        if points == 5: # winning condition
-            pygame.mixer.music.stop()
-            end_anim(screen, True)
-            utils.time_remaining = 0 # Make sure the game stops
-
         if time.time() - seconds_counter > 1:
+            # Timer
             utils.time_remaining -= 1
             seconds_counter = time.time()
 
-        if utils.time_remaining > 0:
+        if utils.time_remaining > 0: # Enough time remaining condition
             utils.run_in_thread(get_data)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -103,52 +96,32 @@ def engine_game(screen, get_data):
             # Microbit input here. Manipulate the data
             # as per minigame's needs. The data comes in
             # an array stored in the utils.data variable.
-            # Example of data manipulation (from engine minigame):
+            # Example of data manipulation (from dinorun minigame):
+            jump = (utils.data[0] == 1)
 
-            # Map the data coming from the microbit to a
-            # scale of 0 to 100.
-            # If the engine is not spinning, the pin is floating
-            # due to the diode protection. Might need adjustement.
-            blow = utils.map(utils.data[3], 570, 1023, 0, 100)
-            if blow > 35:
-                blow_points += 0.05
-            elif blow > 90:
-                blow_points += 0.08
-
-            if int(blow_points) > int(old_blow_points):
-                stage += 1
-                utils.points += 1
-                temp_indicator.change_temp(stage)
-
-            # Animate your sprite, if needed
             animated_sprite.animate()
 
             screen.blit(background, (0, 0))
             animated_sprite_g.draw(screen)
-            screen.blit(main_sprite, (X, Y))
             animated_sprite_g.update()
 
             # Info
-            utils.draw_text(screen, "Directions", utils.width / 2, 322)
+            utils.draw_text(screen, "Insert info here", utils.width / 2, 322)
             utils.draw_points(screen)
             utils.draw_time(screen, utils.time_remaining)
+            utils.run_in_thread(utils.draw_volume(screen))
 
             pygame.display.flip()
-            utils.clock.tick(60)
+            utils.clock.tick(60) # Pick the clock that best suits your game
         else:
-            if points < 5: # If true, the user lost
-                end_anim(screen, False)
-
             pygame.mixer.music.stop()
-            utils.minigame_end(screen)
+            if utils.points - points_counter < 6:
+                # If true, the user lost. Feel free to change the points needed to win
+                decrease_lives()
+                end_anim(screen, False)
+            else:
+                end_anim(screen, True)
 
-            while True:
-                get_data()
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        sys.exit()
-                if type(utils.data[0]) == float and utils.data[0] != 0:
-                    break
+            utils.minigame_end(screen, get_data)
             break
     return
